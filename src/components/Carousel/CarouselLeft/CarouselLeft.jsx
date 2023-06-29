0822839f3ec90a3ae5840b0f326933b78628c385
acinashChandavar7 <avinashChnadavar7@gmail.com>
2023-06-29 21:02:45 +0530

    first commit

---
+++ src/components/Carousel/CarouselLeft/CarouselLeft.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useSwiper } from 'swiper/react';
+
+import { ReactComponent as LeftArrow } from '../../../assets/LeftArrow.svg';
+import styles from './CarouselLeft.module.css';
+
+const CarouselLeft = () => {
+  const swiper = useSwiper();
+  const [isBegining, setIsBegining] = useState(swiper.isBeginning);
+
+  useEffect(() => {
+    swiper.on('slideChange', function () {
+      setIsBegining(swiper.isBeginning);
+    });
+  }, []);
+
+  return (
+    <div className={styles.leftNavigarion}>
+      {isBegining && <LeftArrow onClick={() => swiper.slidePrev()} />}
+    </div>
+  );
+};
+
+export default CarouselLeft;
---
+++ src/components/Carousel/CarouselRight/CarouselRight.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useSwiper } from 'swiper/react';
+
+import { ReactComponent as RightArrow } from '../../../assets/RightArrow.svg';
+
+import styles from './CarouselRight.module.css';
+
+const CarouselRight = () => {
+  const swiper = useSwiper();
+  const [isEnd, setIsEnd] = useState(swiper.isEnd);
+
+  useEffect(() => {
+    swiper.on('slideChange', function () {
+      setIsEnd(swiper.isEnd);
+    });
+  }, []);
+
+  return (
+    <div className={styles.rigthNavigarion}>
+      {!isEnd && <RightArrow onClick={() => swiper.slideNext()} />}
+    </div>
+  );
+};
+
+export default CarouselRight;
